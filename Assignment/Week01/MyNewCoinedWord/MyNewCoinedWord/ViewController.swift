d425178f47304dcee16a3b659baccf5350e31962
조다은 <thisisdana49@gmail.com>
2024-12-29 23:49:11 +0900

    feat: 추천 신조어 클릭 시 검색 되도록 구

--- Assignment/Week01/MyNewCoinedWord/MyNewCoinedWord/ViewController.swift
+++ Assignment/Week01/MyNewCoinedWord/MyNewCoinedWord/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController, UISearchBarDelegate {
 
     let newCoinedWordArray: [String: String] = [
         "중꺾그마": "‘중요한 건 꺾였는데도 그냥 하는 마음’의 줄임말. ‘중꺾마’의 다음 단계이다.",
-        "JMT" : "존맛탱을 영어로, 다시 이니셜만 남긴 고도화 된 표현. 번외로 JMTGR(존맛탱구리)가 있다.",
-        "TMT" : "투머치토커의 줄임말. 말이 과하게 많은 사람을 대상으로 하는 표현이다.",
+        "JMT": "존맛탱을 영어로, 다시 이니셜만 남긴 고도화 된 표현. 번외로 JMTGR(존맛탱구리)가 있다.",
+        "TMT": "투머치토커의 줄임말. 말이 과하게 많은 사람을 대상으로 하는 표현이다.",
         "맛꿀마": "JMT와 같이 맛있음을 표현하는 말. 경상도 사투리가 모티브이다.",
         "웅니": "언니를 아이처럼 귀엽게 부르는 말",
         "가나디": "강아지를 혀 짧은 발음으로 귀엽게 부르는 말. 캐릭터의 이름이기도 하다.",
@@ -23,6 +23,7 @@ class ViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var labelView: UIView!
+    @IBOutlet var titleLabelView: UIView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var contentTextView: UITextView!
 
@@ -36,23 +37,24 @@ class ViewController: UIViewController, UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchInputText(searchBar: searchBar)
     }
-    
+
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
         searchInputText(searchBar: searchBar)
     }
-    
+
     func searchInputText(searchBar: UISearchBar) {
-        guard let searchText = searchBar.text?.uppercased(), !searchText.isEmpty else {
-            titleLabel.text = "검색 결과가 없습니다."
-            contentTextView.text = "목록 중 하나를 선택해볼까요?"
+        guard let searchText = searchBar.text?.uppercased(), !searchText.isEmpty
+        else {
+            titleLabel.text = "✅  검색 결과가 없습니다."
+            contentTextView.text = "목록 중 하나를 클릭해볼까요?"
             return
         }
-        
+
         if let meaning = newCoinedWordArray[searchText] {
-            titleLabel.text = searchText
+            titleLabel.text = "✅  \(searchText)"
             contentTextView.text = meaning
         } else {
-            titleLabel.text = "'\(searchText)'에 대한 검색 결과가 없습니다."
+            titleLabel.text = "✅  '\(searchText)'에 대한 검색 결과가 없습니다."
             contentTextView.text = "목록 중 하나를 선택해볼까요?"
         }
     }
@@ -60,10 +62,10 @@ class ViewController: UIViewController, UISearchBarDelegate {
     @IBAction func tapGestureRecognized(_ sender: Any) {
         view.endEditing(true)
     }
-    
+
     fileprivate func getRandomKeys() -> [String] {
         var keys: Set<String> = []
-        
+
         while keys.count < 4 {
             if let key = newCoinedWordArray.keys.randomElement() {
                 keys.insert(key)
@@ -73,31 +75,56 @@ class ViewController: UIViewController, UISearchBarDelegate {
         return Array(keys).sorted()
     }
 
-    func setUI() {
-        searchBar.tintColor = .accent
+    @objc func wordLabelTapped(_ sender: UITapGestureRecognizer) {
+        guard let tappedLabel = sender.view as? UILabel,
+            let word = tappedLabel.text
+        else { return }
 
+        let meaning = newCoinedWordArray[word]
+        titleLabel.text = "✅  \(word)"
+        contentTextView.text = meaning
+    }
+
+    fileprivate func setUI() {
+        setSearchBarUI()
         setKeywordLabelUI()
         setTextViewUI()
         setLabelViewUI()
     }
 
+    fileprivate func setSearchBarUI() {
+        if let textField = searchBar.value(forKey: "searchField")
+            as? UITextField
+        {
+
+            textField.backgroundColor = .accent
+            textField.textColor = .gray
+
+            if let leftView = textField.leftView as? UIImageView {
+                leftView.image = leftView.image?.withRenderingMode(
+                    .alwaysTemplate)
+                leftView.tintColor = .gray
+            }
+        }
+    }
+
     fileprivate func setKeywordLabelUI() {
-        titleLabel.backgroundColor = .accent
+        titleLabelView.backgroundColor = .accent
         titleLabel.font = .systemFont(ofSize: 18, weight: .bold)
-        titleLabel.text = #"궁금한 신조어를 검색 해보세요!"#
+        titleLabel.text = "✅  궁금한 신조어를 검색 해보세요!"
     }
 
     fileprivate func setTextViewUI() {
         contentTextView.font = .systemFont(ofSize: 18, weight: .regular)
         contentTextView.text = "뭐라고 검색할지 고민된다면 추천 검색어 중에 하나를 골라보세요😉"
     }
-    
+
     fileprivate func setLabelViewUI() {
         labelView.subviews.forEach { $0.removeFromSuperview() }
-        
+
         let randomKeys = getRandomKeys()
         print("random keys: \(randomKeys)")
-        
+
         var xOffset: CGFloat = 0
         let labelHeight: CGFloat = 30
         let labelSpacing: CGFloat = 10
@@ -111,12 +138,16 @@ class ViewController: UIViewController, UISearchBarDelegate {
             wordLabel.backgroundColor = .accent
             wordLabel.layer.masksToBounds = true
             wordLabel.layer.cornerRadius = 5
-            
+
             let labelWidth = wordLabel.intrinsicContentSize.width + 20
-            
-            wordLabel.frame = CGRect(x: xOffset, y: 10, width: labelWidth, height: labelHeight)
+            wordLabel.frame = CGRect(
+                x: xOffset, y: 10, width: labelWidth, height: labelHeight)
+
+            let tapGesture = UITapGestureRecognizer(
+                target: self, action: #selector(wordLabelTapped(_:)))
+            wordLabel.isUserInteractionEnabled = true
+            wordLabel.addGestureRecognizer(tapGesture)
             labelView.addSubview(wordLabel)
-            
             xOffset += labelWidth + labelSpacing
         }
     }
