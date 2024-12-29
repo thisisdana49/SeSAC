5cfd656ccc56c79ec18192cc7707398a40f003ed
조다은 <chodaeun@Danibook.local>
2024-12-29 13:52:29 +0900

    refactor: UI 초기화 로직을 setUI() 메서드로 분리

--- Assignment/Week01/MyMoviePlusProject/MyMoviePlusProject/FirstTabViewController.swift
+++ Assignment/Week01/MyMoviePlusProject/MyMoviePlusProject/FirstTabViewController.swift
@@ -14,36 +14,17 @@ class FirstTabViewController: UIViewController {
     @IBOutlet weak var thumbImage2: UIImageView!
     @IBOutlet weak var thumbImage3: UIImageView!
     
-
     @IBOutlet var badgeViewArray: [UIView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        switchRandomPoster()
-        
-        mainImage.layer.cornerRadius = 10
-        thumbImage1.layer.cornerRadius = 5
-        thumbImage2.layer.cornerRadius = 5
-        thumbImage3.layer.cornerRadius = 5
-        
-        mainImage.layer.masksToBounds = true
-        thumbImage1.layer.masksToBounds = true
-        thumbImage2.layer.masksToBounds = true
-        thumbImage3.layer.masksToBounds = true
-        
-        for index in 0...2 {
-            badgeViewArray[index].addSubview(UIImageView(image: UIImage(named: "badge1")))
-            badgeViewArray[index].addSubview(UIImageView(image: UIImage(named: "blue")))
-            badgeViewArray[index].addSubview(UIImageView(image: UIImage(named: "pink")))
-            
-            badgeViewArray[index].subviews.forEach { $0.isHidden = true }
-        }
+        setUI()
     }
     
     @IBAction func playButtonTapped(_ sender: UIButton) {
         switchRandomPoster()
-        switchRandomMark()
+        switchRandomBadge()
     }
 
     func switchRandomPoster() {
@@ -61,7 +42,7 @@ class FirstTabViewController: UIViewController {
         thumbImage3.image = UIImage(named: posterArray[3])
     }
     
-    func switchRandomMark() {
+    func switchRandomBadge() {
         
         for index in 0...2 {
             let randomInt = Int.random(in: 1...4)
@@ -87,5 +68,46 @@ class FirstTabViewController: UIViewController {
                 print("default")
             }
         }
+    }
+    
+    func setUI() {
+        mainImage.layer.cornerRadius = 10
+        thumbImage1.layer.cornerRadius = 5
+        thumbImage2.layer.cornerRadius = 5
+        thumbImage3.layer.cornerRadius = 5
+        
+        mainImage.layer.masksToBounds = true
+        thumbImage1.layer.masksToBounds = true
+        thumbImage2.layer.masksToBounds = true
+        thumbImage3.layer.masksToBounds = true
+        
+        
+        for index in 0...2 {
+            let badge1 = UIImageView(image: UIImage(named: "badge1"))
+            let badge2 = UIImageView(image: UIImage(named: "badge2"))
+            let badge3 = UIImageView(image: UIImage(named: "badge3"))
+            
+            badgeViewArray[index].addSubview(badge1)
+            badgeViewArray[index].addSubview(badge2)
+            badgeViewArray[index].addSubview(badge3)
+            
+            badge1.translatesAutoresizingMaskIntoConstraints = false
+            badge2.translatesAutoresizingMaskIntoConstraints = false
+            badge3.translatesAutoresizingMaskIntoConstraints = false
+            
+            badge1.topAnchor.constraint(equalTo: badgeViewArray[index].topAnchor).isActive = true
+            badge1.trailingAnchor.constraint(equalTo: badgeViewArray[index].trailingAnchor).isActive = true
+            
+            badge2.bottomAnchor.constraint(equalTo: badgeViewArray[index].bottomAnchor).isActive = true
+            badge2.centerXAnchor.constraint(equalTo: badgeViewArray[index].centerXAnchor).isActive = true
+            
+            badge3.bottomAnchor.constraint(equalTo: badgeViewArray[index].bottomAnchor).isActive = true
+            badge3.centerXAnchor.constraint(equalTo: badgeViewArray[index].centerXAnchor).isActive = true
+            
+            badgeViewArray[index].subviews.forEach { $0.isHidden = true }
+        }
+        
+        switchRandomPoster()
+        switchRandomBadge()
     }
 }
