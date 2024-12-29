93fb075b59f5699cd4d3f19c7f9476d5f85732f1
조다은 <chodaeun@Danibook.local>
2024-12-29 16:58:11 +0900

    feat:  탭제스쳐가 실행되면 키보드 내리도록 구현

--- Assignment/Week01/MyNewCoinedWord/MyNewCoinedWord/ViewController.swift
+++ Assignment/Week01/MyNewCoinedWord/MyNewCoinedWord/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let neologisms: [String: String] = [
+        "중꺾그마": "‘중요한 건 꺾였는데도 그냥 하는 마음’의 줄임말. ‘중꺾마’의 다음 단계이다.",
+        "맛꿀마": "JMT와 같이 맛있음을 표현하는 말. 경상도 사투리가 모티브이다.",
+        "웅니": "언니를 아이처럼 귀엽게 부르는 말",
+        "가나디": "강아지를 혀 짧은 발음으로 귀엽게 부르는 말. 캐릭터의 이름이기도 하다.",
+        "쌈뽕하다": "멋진 것, 혹은 마음에 드는 것을 의미하는 긍정의 표현. 주로 ‘쌈@뽕하다’와 같이 @와 함께 쓰인다.",
+        "럭키비키": "걸그룹 아이브 ‘장원영’이 시작한 유행어로, 운이 좋음을 표현한다.",
+        "복복복": "사랑스럽게 쓰다듬을 때 나는 의성어"
+    ]
+    
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var contentTextView: UITextView!
@@ -17,6 +27,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setUI()
+    }
+    
+    @IBAction func tapGestureRecognized(_ sender: Any) {
+        view.endEditing(true)
     }
     
     func setUI() {
