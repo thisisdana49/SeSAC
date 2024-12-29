e7b1c80f6552ae09fa665e102343b6918432db16
조다은 <chodaeun@Danibook.local>
2024-12-29 15:23:50 +0900

    feat: 버튼 선택 시 동작하는 함수 구현

--- Assignment/Week01/MyMoviePlusProject/MyMoviePlusProject/SecondViewController.swift
+++ Assignment/Week01/MyMoviePlusProject/MyMoviePlusProject/SecondViewController.swift
@@ -10,44 +10,48 @@ import UIKit
 class SecondViewController
 : UIViewController {
     
+    @IBOutlet var buttonArray: [UIButton]!
+    
+    @IBOutlet var contentsLabel: UILabel!
+    @IBOutlet var descLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonTapped(buttonArray[0])
     }
     
-    @IBAction func firstButtonHandler(_ sender: UIButton) {
+    @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isSelected.toggle()
         
         if sender.isSelected {
             sender.backgroundColor = .white
             sender.setTitleColor(.black, for: .normal)
+            for index in buttonArray {
+                if index != sender {
+                    index.isSelected = false
+                    index.backgroundColor = .black
+                    index.setTitleColor(.white, for: .normal)
+                }
+            }
         } else {
             sender.backgroundColor = .black
             sender.setTitleColor(.white, for: .normal)
         }
-    }
-    
-    @IBAction func secondButtonHandler(_ sender: UIButton) {
-        sender.isSelected.toggle()
         
-        if sender.isSelected {
-            sender.backgroundColor = .white
-            sender.setTitleColor(.black, for: .normal)
-        } else {
-            sender.backgroundColor = .black
-            sender.setTitleColor(.white, for: .normal)
-        }
-    }
-    
-    @IBAction func thirdButtonHandler(_ sender: UIButton) {
-        sender.isSelected.toggle()
-        
-        if sender.isSelected {
-            sender.backgroundColor = .white
-            sender.setTitleColor(.black, for: .normal)
-        } else {
-            sender.backgroundColor = .black
-            sender.setTitleColor(.white, for: .normal)
+        switch sender {
+            case buttonArray[0]:
+            contentsLabel.text = "공개 예정"
+            descLabel.text = "곧 공개 될 콘텐츠를 소개합니다"
+        case buttonArray[1]:
+            contentsLabel.text = "모두의 인기 콘텐츠"
+            descLabel.text = "남녀노소 즐길 수 있는 콘텐츠를 소개합니다"
+        case buttonArray[2]:
+            contentsLabel.text = "TOP 10 시리즈"
+            descLabel.text = "최근 핫한 TOP 10 시리즈를 소개합니다"
+        default:
+            self.contentsLabel.text = "이런! 찾으시는 작품이 없습니다."
+            self.descLabel.text = "다른 영화, 시리즈, 배우, 감독 또는 장르를 검색해 보세요."
         }
     }
 }
