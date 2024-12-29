c029f12471daaacbf965c958927e09de798fca14
조다은 <thisisdana49@gmail.com>
2024-12-29 22:20:39 +0900

    feat: 대소문자 구분 없이 검색되는 기능 구

--- Assignment/Week01/MyNewCoinedWord/MyNewCoinedWord/ViewController.swift
+++ Assignment/Week01/MyNewCoinedWord/MyNewCoinedWord/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController, UISearchBarDelegate {
 
     let newCoinedWordArray: [String: String] = [
         "중꺾그마": "‘중요한 건 꺾였는데도 그냥 하는 마음’의 줄임말. ‘중꺾마’의 다음 단계이다.",
+        "JMT" : "존맛탱을 영어로, 다시 이니셜만 남긴 고도화 된 표현. 번외로 JMTGR(존맛탱구리)가 있다.",
+        "TMT" : "투머치토커의 줄임말. 말이 과하게 많은 사람을 대상으로 하는 표현이다.",
         "맛꿀마": "JMT와 같이 맛있음을 표현하는 말. 경상도 사투리가 모티브이다.",
         "웅니": "언니를 아이처럼 귀엽게 부르는 말",
         "가나디": "강아지를 혀 짧은 발음으로 귀엽게 부르는 말. 캐릭터의 이름이기도 하다.",
@@ -39,7 +41,7 @@ class ViewController: UIViewController, UISearchBarDelegate {
     }
     
     func searchInputText(searchBar: UISearchBar) {
-        guard let searchText = searchBar.text, !searchText.isEmpty else {
+        guard let searchText = searchBar.text?.uppercased(), !searchText.isEmpty else {
             titleLabel.text = "검색 결과가 없습니다."
             contentTextView.text = "목록 중 하나를 선택해볼까요?"
             return
