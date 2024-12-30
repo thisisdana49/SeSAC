d61bba7e6d628c9ea15619ffa8ab297fe1dc4a72
조다은 <thisisdana49@gmail.com>
2024-12-30 20:08:09 +0900

    design: NicknameViewController UI 구성

--- Assignment/Week02/MyBMICalculator/MyBMICalculator/ViewController.swift
+++ Assignment/Week02/MyBMICalculator/MyBMICalculator/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NicknameViewController: UIViewController {
 
+    @IBOutlet var guideTitleLabel: UILabel!
+    @IBOutlet var guideContentLabel: UILabel!
+    @IBOutlet var nicknameTextField: UITextField!
+    @IBOutlet var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
     }
 
-
+    fileprivate func setUI() {
+        guideTitleLabel.numberOfLines = 0
+        guideTitleLabel.text = "체중 관리,\n함께 해볼까요?"
+        guideTitleLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        guideContentLabel.numberOfLines = 0
+        guideContentLabel.text = "먼저 닉네임을\n입력해 주세요."
+        guideContentLabel.font = UIFont.systemFont(ofSize: 16)
+        guideContentLabel.textColor = .lightGray
+        
+        nextButton.setTitle("다음", for: .normal)
+        nextButton.tintColor = .white
+        nextButton.backgroundColor = .systemBlue
+        nextButton.layer.cornerRadius = 10
+    }
 }
 
