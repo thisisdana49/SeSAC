c5821e7c45eb0570fe474795ea84592dc39c9eff
조다은 <thisisdana49@gmail.com>
2024-12-30 17:08:11 +0900

    feat: UserDefaults 저장 및 불러오기 기능

--- Practice/Week02/MySecondApp/MySecondApp/NumberViewController.swift
+++ Practice/Week02/MySecondApp/MySecondApp/NumberViewController.swift
@@ -9,21 +9,43 @@ import UIKit
 
 class NumberViewController: UIViewController {
 
+    @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(NSHomeDirectory())
+        
+        guard let savedNumber = UserDefaults.standard.value(forKey: "numbers") else {
+            resultLabel.text = "No Data"
+            return
+        }
+        
+        resultLabel.text = "It's \(savedNumber)"
+    }
 
-        // Do any additional setup after loading the view.
+    // 실시간으로 레이블에 내용 반영
+    // 입력한 내용이 숫자라면 숫자 그대로 출력
+    // 입력한 내용이 문자라면 숫자가 아닙니다 출력
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        guard let inputText = numberTextField.text, let doubleNumber = Double(inputText) else {
+            resultLabel.text = numberTextField.text?.count == 0 ? "No Input" : "It's not a number"
+            return
+        }
+
+        let intNumber = Int(doubleNumber)
+        
+        if doubleNumber == Double(intNumber) {
+            resultLabel.text = "It's \(intNumber)"
+        } else {
+            resultLabel.text = "It's \(doubleNumber)"
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        // 텍스트필드에 적었던 내용을 영구적으로 저장
+        UserDefaults.standard.set(numberTextField.text, forKey: "number")
+        print(#function)
     }
-    */
-
 }
--- Practice/Week02/MySecondApp/MySecondApp/ViewController.swift
+++ Practice/Week02/MySecondApp/MySecondApp/ViewController.swift
@@ -9,11 +9,98 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var wordTextField: UITextField!
+    @IBOutlet var suggestButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // UIButton titleLabel is read-only property!!!
+        print(suggestButton.titleLabel!)
+        resultLabel.isUserInteractionEnabled = true
+        
+        aboutOptional()
     }
-
-
+    
+    // Did End on Exit
+    // Outlet 연결 시 꼭 'Did End on Exit'를 선택해야함.
+    // UITextField text, UILabel text : String?
+    @IBAction func textFieldReturnKeyTapped(_ sender: UITextField) {
+        // UITextField text에 아무리 nil을 넣어도 빈 문자열을 토해낸다.
+        // text: String이기 때문에 애초에 nil을 받을 수 없다.
+        // !(Forced Unwrapping)을 해도 꺼지지 않는다!
+        let text = wordTextField.text!.uppercased()
+        
+        // TODO: 공백이 입력 되었을 때도 추가하기
+        if text.count == 0 {
+            resultLabel.text = "검색어를 입력해주세요."
+        } else if text == "jmt" {
+            resultLabel.text = "존맛탱"
+        } else {
+            resultLabel.text = "\(text)를 입력했어요."
+        }
+    }
+    
+    @IBAction func labelTapped(_ sender: UITapGestureRecognizer) {
+        print(#function)
+    }
+    
+    
+    
+    
+    func aboutOptional() {
+        var nickname: String? = "다우니맛"
+        var age: Int? = 100
+        
+        // optional이 아닌 곳에 nil을 넣을 수 X
+        nickname = nil
+        
+        
+        // 1. 조건문을 통해 옵셔널 언래핑 {
+        if nickname == nil {
+            print("닉네임에 문제가 있어요")
+        } else {
+            print("\(nickname!)으로 설정합니다.")
+        }
+        
+        if nickname != nil {
+            print("\(nickname!)으로 설정합니다.")
+        } else {
+            print("닉네임에 문제가 있어요")
+        }
+        
+        
+        // 2. Optional Binding: if let
+        if let nickname = nickname {
+            print("\(nickname)으로 설정합니다.")
+        } else {
+            print("닉네임에 문제가 있어요")
+        }
+        
+        // Swift5.7 if let shorthand
+        if let age {
+            print("\(age)살이군요")
+        } else {
+            print("네? 몇 살이라구요?")
+        }
+        
+        // 2. Optional Binding: guard let
+        guard let age else {
+            print("Failed to binding optional, age is nil")
+            return
+        }
+        print(age)
+        
+        // 3. ?? Optional Coalescing
+        print(nickname ?? "Dana")
+        
+        // 4. Optional Chaining
+        wordTextField?.text?.count
+        
+        if let text = wordTextField.text {
+            print(text.count)
+        }
+    }
 }
 
