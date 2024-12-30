4ebb7c7261005feed5e8e0ad0f6ced9f1d2a8f9d
조다은 <thisisdana49@gmail.com>
2024-12-30 13:25:56 +0900

    chore: 2주차 실습 프로젝트 생성

---
+++ Practice/Week02/MySecondApp/MySecondApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by 조다은 on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
