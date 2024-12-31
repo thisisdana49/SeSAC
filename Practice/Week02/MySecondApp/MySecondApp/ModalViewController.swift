0c449f044393e15d056574d784c155328ac00201
조다은 <thisisdana49@gmail.com>
2024-12-31 12:05:11 +0900

    feat: ViewController 생명 주기 실습

--- Practice/Week02/MySecondApp/MySecondApp/ModalViewController.swift
+++ Practice/Week02/MySecondApp/MySecondApp/ModalViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class ModalViewController: UIViewController {
 
+    let colorList: [UIColor] = [.red, .green, .blue, .yellow]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = colorList.randomElement()
+        print(self, #function)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(self, #function)
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(self, #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(self, #function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(self, #function)
+    }
 }
