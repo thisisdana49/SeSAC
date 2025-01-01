0ddbd28449ffcbe04fafb15e10a59767388027eb
조다은 <thisisdana49@gmail.com>
2025-01-01 15:34:18 +0900

    feat: 메모리 사용량 및 unwind 실

--- Practice/Week02/MySecondApp/MySecondApp/PracticeViewController.swift
+++ Practice/Week02/MySecondApp/MySecondApp/PracticeViewController.swift
@@ -14,7 +14,6 @@ class PracticeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = colorList.randomElement()
         print(self, #function)
     }
     
@@ -23,7 +22,14 @@ class PracticeViewController: UIViewController {
         print(self, #function)
     }
     
+    // iOS 13.0+
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        print(self, #function)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
+        view.backgroundColor = colorList.randomElement()
         super.viewDidAppear(animated)
         print(self, #function)
     }
@@ -36,5 +42,10 @@ class PracticeViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         print(self, #function)
+    }
+    
+    // 스토리 보드에서 연결하는 게 X, 코드로 직접 타이핑
+    @IBAction func unwindToPracticeViewController(_ sender: UIStoryboardSegue) {
+        print(#function)
     }
 }
