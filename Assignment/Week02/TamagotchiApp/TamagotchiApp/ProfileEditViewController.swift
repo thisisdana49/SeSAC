5530625396789b35a028032b1cdb8f61adae573a
조다은 <thisisdana49@gmail.com>
2025-01-02 01:16:00 +0900

    feat: 대장 이름 변경 없을 시 저장 버튼 비활성화 기능

--- Assignment/Week02/TamagotchiApp/TamagotchiApp/ProfileEditViewController.swift
+++ Assignment/Week02/TamagotchiApp/TamagotchiApp/ProfileEditViewController.swift
@@ -13,25 +13,27 @@ class ProfileEditViewController: UIViewController {
     @IBOutlet var nicknameTextField: UITextField!
     @IBOutlet var guideLabel: UILabel!
     
+    var userNickname: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUserInformation()
         setUI()
-        
-        navigationItem.rightBarButtonItem = saveNicknameButton
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveNickname))
     }
 
     @IBAction func nicknameTextFieldDidChange(_ sender: UITextField) {
         let textCount = sender.text?.count ?? 0
-        if textCount >= 2 && textCount <= 6 {
-            saveNicknameButton.isEnabled = true
-            guideLabel.text = ""
-        } else {
-            saveNicknameButton.isEnabled = false
+        if textCount < 2 || textCount > 6 {
+            navigationItem.rightBarButtonItem?.isEnabled = false
             guideLabel.text = "닉네임이 2글자 이상 6글자 이하여야 합니다."
             guideLabel.font = UIFont.systemFont(ofSize: 12)
             guideLabel.textColor = .red
+        } else if sender.text == userNickname {
+            navigationItem.rightBarButtonItem?.isEnabled = false
+        } else {
+            navigationItem.rightBarButtonItem?.isEnabled = true
+            guideLabel.text = ""
         }
     }
 
@@ -41,11 +43,19 @@ class ProfileEditViewController: UIViewController {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
+    func setUserInformation() {
+        guard let userNickname = UserDefaults.standard.string(forKey: "userNickname") else { return }
+        self.userNickname = userNickname
+    }
+    
     func setUI() {
         self.navigationController?.navigationBar.topItem?.title = "설정"
-        saveNicknameButton.title = "저장"
-        saveNicknameButton.isEnabled = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveNickname))
+        navigationItem.rightBarButtonItem?.isEnabled = false
         
-        guideLabel.text = ""
+        view.backgroundColor = .base
+        
+        nicknameTextField.text = userNickname
+        nicknameTextField.placeholder = "대장의 이름을 입력해 주세요."
     }
 }
