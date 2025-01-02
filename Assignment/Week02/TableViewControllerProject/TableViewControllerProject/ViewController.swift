64728bbaed3762338c1f6718dbd977181a28b374
조다은 <thisisdana49@gmail.com>
2025-01-02 20:03:55 +0900

    chore: 프로젝트 생성 및 기본 세팅 작업

---
+++ Assignment/Week02/TableViewControllerProject/TableViewControllerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewControllerProject
+//
+//  Created by 조다은 on 1/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
