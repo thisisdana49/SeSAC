54e6e7e4ae1c31504f428f4b82191b2b4147ff17
조다은 <thisisdana49@gmail.com>
2025-01-03 00:49:25 +0900

    feat: TextField에 입력 후 추가 버튼 클릭 시 TableView에 추가, 즉각 반영 되는 기능

--- Assignment/Week02/TableViewControllerProject/TableViewControllerProject/CaseThreeTableViewController.swift
+++ Assignment/Week02/TableViewControllerProject/TableViewControllerProject/CaseThreeTableViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class CaseThreeTableViewController: UITableViewController {
 
+    @IBOutlet var todoTextField: UITextField!
+    @IBOutlet var addButton: UIButton!
+    
     var todoArray: [String] = ["그립톡 구매하기", "사이다 사기", "아이패드 케이스 최저가 알아보기", "양말"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "쇼핑"
+        tableView.separatorStyle = .none
+        todoTextField.placeholder = "무엇을 구매하실 건가요?"
+        todoTextField.backgroundColor = .systemGray6
+        
+        addButton.setTitle("추가", for: .normal)
+        addButton.backgroundColor = .systemGray5
+        addButton.layer.cornerRadius = 5
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -31,12 +41,34 @@ class CaseThreeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        // tag를 0으로 줬더니 view의 색이 변하지 않았다. cell의 속성 tag 기본값이 0이라서 그런 것일까?
+        let backgroundView = cell.viewWithTag(100)
+        let checkboxImage = cell.viewWithTag(1) as? UIImageView
+        let todoLabel = cell.viewWithTag(2) as? UILabel
+        let bookmarkImage = cell.viewWithTag(3) as? UIImageView
+        
+        backgroundView?.backgroundColor = .systemGray6
+        backgroundView?.layer.cornerRadius = 10
+        checkboxImage?.image = UIImage(systemName: "checkmark.square")?.withTintColor(.black).withRenderingMode(.alwaysOriginal)
+        todoLabel?.text = todoArray[indexPath.row]
+        todoLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        bookmarkImage?.image = UIImage(systemName: "star")?.withTintColor(.black).withRenderingMode(.alwaysOriginal)
 
         return cell
     }
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+    }
+    
+    @IBAction func addTodoButtonTapped(_ sender: UIButton) {
+        todoArray.append(todoTextField.text ?? "")
+        todoTextField.text = ""
+        tableView.reloadData()
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
