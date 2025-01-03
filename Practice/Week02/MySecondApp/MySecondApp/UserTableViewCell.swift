9940e9ec05add0b53b9c39c1667ca4864b9f60eb
조다은 <thisisdana49@gmail.com>
2025-01-03 16:33:15 +0900

    feat: UITableViewController에 Custom Cell로 구현

---
+++ Practice/Week02/MySecondApp/MySecondApp/UserTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserTableViewCell.swift
+//  MySecondApp
+//
+//  Created by 조다은 on 1/3/25.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var aliasLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+}
