2750db4bd1a1ab03e88c58ee1d875c22da968c47
조다은 <thisisdana49@gmail.com>
2025-01-03 19:54:04 +0900

    design: Main 화면의 UITabelViewCell을 AutoLayout으로 구현

---
+++ Assignment/Week02/TourGuideApp/TourGuideApp/MainTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MainTableViewCell.swift
+//  TourGuideApp
+//
+//  Created by 조다은 on 1/3/25.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
