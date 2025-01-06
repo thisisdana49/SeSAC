9fe80b3ab7a4d417e1388d4125a4cc4a87e1360d
조다은 <thisisdana49@gmail.com>
2025-01-06 19:07:35 +0900

    refactor: TableViewController의 코드 중 UI 관련한 로직은 TableViewCell로 이동

--- Assignment/Week03/TourGuideApp/TourGuideApp/FirstTableViewCell.swift
+++ Assignment/Week03/TourGuideApp/TourGuideApp/FirstTableViewCell.swift
@@ -8,21 +8,56 @@
 import UIKit
 
 class FirstTableViewCell: UITableViewCell {
-
-    @IBOutlet var mainImageView: UIImageView!
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var subTitleLabel: UILabel!
-    @IBOutlet var dateLabel: UILabel!
+    static let identifier = "FirstTableViewCell"
+    
+    @IBOutlet private var mainImageView: UIImageView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var subTitleLabel: UILabel!
+    @IBOutlet private var dateLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configure()
     }
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func configure() {
+        mainImageView.contentMode = .scaleAspectFill
+        mainImageView.layer.cornerRadius = 20
+        
+        titleLabel.numberOfLines = 0
+        titleLabel.lineBreakMode = .byWordWrapping
+        titleLabel.lineBreakStrategy = .hangulWordPriority
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
 
-        // Configure the view for the selected state
+        subTitleLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        subTitleLabel.textColor = .systemGray2
+        
+        dateLabel.textAlignment = .right
+        dateLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        dateLabel.textColor = .systemGray3
     }
+    
+    func configureData(row: Magazine) {
+        if let img = URL(string: row.photo_image) {
+            mainImageView.kf.setImage(with: img)
+        } else {
+            mainImageView.image = .no
+        }
+
+        titleLabel.text = row.title
+        subTitleLabel.text = row.subtitle
+
+        let formatedDate = formatStringToDateString(row.date)
+        dateLabel.text = formatedDate
+    }
+    
+    func formatStringToDateString(_ date: String) -> String {
+        let dateArray = Array(date)
+        let formatedDate = "\(dateArray[0])\(dateArray[1])년 \(dateArray[2])\(dateArray[3])월 \(dateArray[4])\(dateArray[5])일"
+        return formatedDate
+    }
+
 
 }
