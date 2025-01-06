5056f319a630c1cef304cb6285f397257435e8dd
조다은 <thisisdana49@gmail.com>
2025-01-07 01:38:09 +0900

    feat: UITextField에서 검색한 단어로 필터링하여 보여주도록 구현

--- Assignment/Week03/TourGuideApp/TourGuideApp/FourTableViewController.swift
+++ Assignment/Week03/TourGuideApp/TourGuideApp/FourTableViewController.swift
@@ -7,83 +7,138 @@
 
 import UIKit
 
-class FourTableViewController: UITableViewController {
-    let cityInfo = CityInfo() // CityInfo의 인스턴스 생성
+class FourTableViewController: UITableViewController, UITextFieldDelegate {
+    let cityInfo = CityInfo()  // CityInfo의 인스턴스 생성
 
     let allCities = CityInfo().city
     let domesticCities = CityInfo().city.filter { $0.domestic_travel }
     let foreignCities = CityInfo().city.filter { !$0.domestic_travel }
-    
+
+    var isSearching: Bool = false
+    var filteredCities: [City] = []
+
     @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
     @IBOutlet var segmentedControl: UISegmentedControl!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setUI()
+
+        searchTextField.delegate = self
     }
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch(segmentedControl.selectedSegmentIndex) {
-        case 0:
-            return allCities.count
-        case 1:
-            return domesticCities.count
-        case 2:
-            return foreignCities.count
-        default:
-            return allCities.count
+    override func tableView(
+        _ tableView: UITableView, numberOfRowsInSection section: Int
+    ) -> Int {
+        if isSearching {
+            return filteredCities.count
+        } else {
+            switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                return allCities.count
+            case 1:
+                return domesticCities.count
+            case 2:
+                return foreignCities.count
+            default:
+                return allCities.count
+            }
         }
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: FourTableViewCell.identifier, for: indexPath) as! FourTableViewCell
+    override func tableView(
+        _ tableView: UITableView, cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell =
+            tableView.dequeueReusableCell(
+                withIdentifier: FourTableViewCell.identifier, for: indexPath)
+            as! FourTableViewCell
         let city: City
-        
-        switch(segmentedControl.selectedSegmentIndex) {
-        case 0:
-            city = allCities[indexPath.row]
-        case 1:
-            city = domesticCities[indexPath.row]
-        case 2:
-            city = foreignCities[indexPath.row]
-        default:
-            city = allCities[indexPath.row]
-            break
+
+        if isSearching {
+            city = filteredCities[indexPath.row]
+        } else {
+            switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                city = allCities[indexPath.row]
+            case 1:
+                city = domesticCities[indexPath.row]
+            case 2:
+                city = foreignCities[indexPath.row]
+            default:
+                city = allCities[indexPath.row]
+            }
         }
-        
+
         cell.configureData(row: city)
-        
+
         return cell
     }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+    override func tableView(
+        _ tableView: UITableView, heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
         return 130
     }
-    
+
     @IBAction func segmentedControlSelected(_ sender: UISegmentedControl) {
-        switch(segmentedControl.selectedSegmentIndex) {
-        case 0:
+        isSearching = false
+        searchTextField.text = ""
+        tableView.reloadData()
+    }
+
+    // TextField Delegate
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        guard let searchText = textField.text, !searchText.isEmpty else {
+            isSearching = false
+            filteredCities = []
             tableView.reloadData()
-        case 1:
-            tableView.reloadData()
-        case 2:
-            tableView.reloadData()
-        default:
-            break
+            return
         }
+
+        isSearching = true
+
+        let currentCities: [City]
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            currentCities = allCities
+        case 1:
+            currentCities = domesticCities
+        case 2:
+            currentCities = foreignCities
+        default:
+            currentCities = allCities
+        }
+
+        filteredCities = currentCities.filter {
+            $0.city_name.contains(searchText)
+                || $0.city_english_name.contains(searchText)
+                || $0.city_explain.contains(searchText)
+        }
+
+        tableView.reloadData()
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+
     private func setUI() {
         self.navigationItem.title = "인기 도시"
-        
+
         // XIB cell
         let nib = UINib(nibName: FourTableViewCell.identifier, bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: FourTableViewCell.identifier)
+        tableView.register(
+            nib, forCellReuseIdentifier: FourTableViewCell.identifier)
         tableView.separatorStyle = .none
-        
+
+        searchTextField.placeholder = "어디로 떠나볼까요? 😎"
+        searchTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        searchButton.setImage(UIImage(systemName: "magnifyingglass")?.withTintColor(.systemGray3).withRenderingMode(.alwaysOriginal), for: .normal)
         segmentedControl.setTitle("모두", forSegmentAt: 0)
         segmentedControl.setTitle("국내", forSegmentAt: 1)
         segmentedControl.insertSegment(withTitle: "해외", at: 2, animated: false)
