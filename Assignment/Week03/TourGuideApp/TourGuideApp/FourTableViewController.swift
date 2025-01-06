2f8442077b8a7fbee1c0ab0a6ca46dca8b6dcb1e
조다은 <thisisdana49@gmail.com>
2025-01-07 01:06:29 +0900

    feat: UISegmentedControl로 선택된 세그먼트에 따라 해당하는 도시만 필터링하여 TableView에 표시

--- Assignment/Week03/TourGuideApp/TourGuideApp/FourTableViewController.swift
+++ Assignment/Week03/TourGuideApp/TourGuideApp/FourTableViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class FourTableViewController: UITableViewController {
-    let cities = CityInfo().city
+    let cityInfo = CityInfo() // CityInfo의 인스턴스 생성
+
+    let allCities = CityInfo().city
+    let domesticCities = CityInfo().city.filter { $0.domestic_travel }
+    let foreignCities = CityInfo().city.filter { !$0.domestic_travel }
     
     @IBOutlet var searchTextField: UITextField!
-    @IBOutlet var travelFilterSegmentedControl: UISegmentedControl!
+    @IBOutlet var segmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +26,33 @@ class FourTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities.count
+        switch(segmentedControl.selectedSegmentIndex) {
+        case 0:
+            return allCities.count
+        case 1:
+            return domesticCities.count
+        case 2:
+            return foreignCities.count
+        default:
+            return allCities.count
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FourTableViewCell.identifier, for: indexPath) as! FourTableViewCell
-        let city = cities[indexPath.row]
+        let city: City
+        
+        switch(segmentedControl.selectedSegmentIndex) {
+        case 0:
+            city = allCities[indexPath.row]
+        case 1:
+            city = domesticCities[indexPath.row]
+        case 2:
+            city = foreignCities[indexPath.row]
+        default:
+            city = allCities[indexPath.row]
+            break
+        }
         
         cell.configureData(row: city)
         
@@ -38,6 +63,19 @@ class FourTableViewController: UITableViewController {
         return 130
     }
     
+    @IBAction func segmentedControlSelected(_ sender: UISegmentedControl) {
+        switch(segmentedControl.selectedSegmentIndex) {
+        case 0:
+            tableView.reloadData()
+        case 1:
+            tableView.reloadData()
+        case 2:
+            tableView.reloadData()
+        default:
+            break
+        }
+    }
+    
     private func setUI() {
         self.navigationItem.title = "인기 도시"
         
@@ -46,8 +84,8 @@ class FourTableViewController: UITableViewController {
         tableView.register(nib, forCellReuseIdentifier: FourTableViewCell.identifier)
         tableView.separatorStyle = .none
         
-        travelFilterSegmentedControl.setTitle("모두", forSegmentAt: 0)
-        travelFilterSegmentedControl.setTitle("국내", forSegmentAt: 1)
-        travelFilterSegmentedControl.insertSegment(withTitle: "해외", at: 2, animated: false)
+        segmentedControl.setTitle("모두", forSegmentAt: 0)
+        segmentedControl.setTitle("국내", forSegmentAt: 1)
+        segmentedControl.insertSegment(withTitle: "해외", at: 2, animated: false)
     }
 }
