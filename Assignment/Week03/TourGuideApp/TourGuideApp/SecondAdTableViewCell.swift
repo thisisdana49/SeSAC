91ddcb45e81db2bcc99d35ff5d4e7f42350279c2
조다은 <thisisdana49@gmail.com>
2025-01-06 20:08:36 +0900

    refactor: 두번째 탭 TableViewController의 코드 중 UI 관련한 로직은 TableViewCell로 이동

--- Assignment/Week03/TourGuideApp/TourGuideApp/SecondAdTableViewCell.swift
+++ Assignment/Week03/TourGuideApp/TourGuideApp/SecondAdTableViewCell.swift
@@ -8,20 +8,39 @@
 import UIKit
 
 class SecondAdTableViewCell: UITableViewCell {
-
+    static var identifier = "SecondAdTableViewCell"
+    
     @IBOutlet var baseView: UIView!
     @IBOutlet var contentLabel: UILabel!
     @IBOutlet var badgeLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configure()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    private func configure() {
+        self.separatorInset = UIEdgeInsets(top: 0, left: UIScreen.main.bounds.width, bottom: 0, right: 0)
+        
+        let colors: [UIColor] = [.pointPink, .pointGreen]
+        let color = colors.randomElement()
+        baseView.backgroundColor = color
+        baseView.layer.cornerRadius = 10
+        
+        contentLabel.textAlignment = .center
+        contentLabel.numberOfLines = 0
+        contentLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        
+        badgeLabel.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        badgeLabel.textAlignment = .center
+        badgeLabel.backgroundColor = .white
+        badgeLabel.layer.cornerRadius = 10
+        badgeLabel.layer.masksToBounds = true
+        badgeLabel.text = "AD"
     }
-
+    
+    func configureData(row: Travel) {
+        contentLabel.text = row.title
+    }
 }
--- Assignment/Week03/TourGuideApp/TourGuideApp/SecondTableViewCell.swift
+++ Assignment/Week03/TourGuideApp/TourGuideApp/SecondTableViewCell.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SecondTableViewCell: UITableViewCell {
-
+    static var identifier = "SecondTableViewCell"
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var descLabel: UILabel!
     @IBOutlet var gradeLabel: UILabel!
@@ -18,13 +19,69 @@ class SecondTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configure()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        travelImageView.image = .no
+    }
+    
+    private func configure() {
+        self.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        titleLabel.textColor = .darkGray
+        
+        descLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        descLabel.textColor = .systemGray
+        
+        travelImageView.contentMode = .scaleAspectFill
+        travelImageView.layer.cornerRadius = 8
+    }
+    
+    func configureData(row: Travel) {
+        titleLabel.text = row.title
+            
+        descLabel.text = row.description
+        setStarsLabel(from: row.grade!, to: gradeLabel)
+        setFormattedNumberLabel(from: row.save!, to: saveLabel)
+        
+        if let imgURL = URL(string: row.travel_image!) {
+            travelImageView.kf.setImage(with: imgURL)
+        } else {
+            travelImageView.image = .no
+        }
+        
+        likeButton.setImage(UIImage(systemName: row.like! ?  "heart" : "heart.fill")?.withTintColor(.white).withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+                
+    func setStarsLabel(from doubleValue: Double, to label: UILabel) {
+        let randomNum = String(Int.random(in: 1...2000))
+        let starCount = Int(round(doubleValue))
+        let yellowStars = NSAttributedString(string: String(repeating: "★", count: starCount), attributes: [.foregroundColor: UIColor.pointYellow])
+        
+        let remaingCount = max(5 - starCount, 0)
+        let grayStarts = NSAttributedString(string: String(repeating: "★", count: remaingCount), attributes: [.foregroundColor: UIColor.systemGray6, .font: UIFont.systemFont(ofSize: 15)])
+        
+        let reviewCount = " (\(randomNum))"
+        let reviewString = NSAttributedString(string: reviewCount, attributes: [.foregroundColor: UIColor.systemGray3, .font: UIFont.systemFont(ofSize: 15)])
+        
+        let combinedStars = NSMutableAttributedString()
+        combinedStars.append(yellowStars)
+        combinedStars.append(grayStarts)
+        combinedStars.append(reviewString)
+        
+        label.attributedText = combinedStars
     }
 
+    func setFormattedNumberLabel(from intValue: Int, to label: UILabel) {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let formattedSave = numberFormatter.string(for: intValue) ?? ""
+        
+        let saveString = NSAttributedString(string: "· 저장 \(formattedSave)", attributes: [.foregroundColor: UIColor.systemGray3, .font: UIFont.systemFont(ofSize: 15)])
+        
+        label.attributedText = saveString
+    }
 }
