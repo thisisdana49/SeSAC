6dac0a8306e12921120c008deeee6558656c75cf
조다은 <thisisdana49@gmail.com>
2025-01-07 00:37:00 +0900

    design: 검색 TextField, 페이지 필터링 SegmentedControl 추가

--- Assignment/Week03/TourGuideApp/TourGuideApp/FourTableViewController.swift
+++ Assignment/Week03/TourGuideApp/TourGuideApp/FourTableViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 class FourTableViewController: UITableViewController {
     let cities = CityInfo().city
     
+    @IBOutlet var searchTextField: UITextField!
     @IBOutlet var travelFilterSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // XIB cell
-        let nib = UINib(nibName: FourTableViewCell.identifier, bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: FourTableViewCell.identifier)
-        tableView.separatorStyle = .none
+        setUI()
     }
 
     // MARK: - Table view data source
@@ -38,5 +36,18 @@ class FourTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 130
+    }
+    
+    private func setUI() {
+        self.navigationItem.title = "인기 도시"
+        
+        // XIB cell
+        let nib = UINib(nibName: FourTableViewCell.identifier, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: FourTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        
+        travelFilterSegmentedControl.setTitle("모두", forSegmentAt: 0)
+        travelFilterSegmentedControl.setTitle("국내", forSegmentAt: 1)
+        travelFilterSegmentedControl.insertSegment(withTitle: "해외", at: 2, animated: false)
     }
 }
