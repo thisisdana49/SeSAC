a77b7a33a5097ad9f05249d70b62891e14cfbc1e
조다은 <thisisdana49@gmail.com>
2025-01-08 22:57:14 +0900

    feat: 식당 위치 annotation 표시 및 필터링하여 표시하는 기능 구현 #27

--- Assignment/Week03/TastyMap/TastyMap/MainViewController.swift
+++ Assignment/Week03/TastyMap/TastyMap/MainViewController.swift
@@ -8,16 +8,80 @@
 import UIKit
 import MapKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, MKMapViewDelegate {
+    var restaurants = RestaurantList().restaurantArray
+    var filteredRestaurants: [Restaurant] = []
+    let categories = ["전체 보기", "한식🍚", "양식🍕", "일식🍣", "중식🍜", "분식🍢", "비건🌿", "카페☕️"]
+    var annotations: [MKPointAnnotation] = [] {
+        didSet {
+            print("annotation did set")
+            mapView.removeAnnotations(oldValue)
+            for annotation in annotations {
+                mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
     @IBOutlet var categoryScrollView: UIView!
     @IBOutlet var mapView: MKMapView!
-    let categories = ["전체 보기", "한식", "양식", "일식", "중식", "분식", "비건", "카페"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        
         setUI()
+        configurationMapView()
     }
 
+    private func configurationMapView() {
+        for restaurant in restaurants {
+            let center = CLLocationCoordinate2D(latitude: 37.65370, longitude: 127.04740)
+            mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: restaurant.latitude, longitude: restaurant.longitude)
+            annotation.title = "\(restaurant.name)"
+            annotations.append(annotation)
+        }
+    }
+    
+    private func showAnnotations() {
+        var newAnnotations: [MKPointAnnotation] = []
+        for restaurant in filteredRestaurants {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: restaurant.latitude, longitude: restaurant.longitude)
+            annotation.title = "\(restaurant.name)"
+            newAnnotations.append(annotation)
+        }
+        annotations = newAnnotations
+    }
+
+    @objc
+    func categoryButtonTapped(_ sender: UIButton) {
+        print(#function, sender.tag)
+        filteredRestaurants.removeAll()
+        switch sender.tag {
+        case 0:
+            filteredRestaurants = restaurants
+        case 1:
+            filteredRestaurants = restaurants.filter { $0.category == "한식" }
+        case 2:
+            filteredRestaurants = restaurants.filter { $0.category == "양식" }
+        case 3:
+            filteredRestaurants = restaurants.filter { $0.category == "일식" }
+        case 4:
+            filteredRestaurants = restaurants.filter { $0.category == "중식" }
+        case 5:
+            filteredRestaurants = restaurants.filter { $0.category == "분식" }
+        case 6:
+            filteredRestaurants = restaurants.filter { $0.category == "비건" }
+        case 7:
+            filteredRestaurants = restaurants.filter { $0.category == "카페" }
+        default: break
+        }
+        showAnnotations()
+    }
+    
     private func setUI() {
         let stackView = UIStackView()
         stackView.axis = .horizontal
@@ -25,14 +89,19 @@ class MainViewController: UIViewController {
         stackView.alignment = .center
         stackView.distribution = .equalSpacing
         stackView.translatesAutoresizingMaskIntoConstraints = false
-
-        for category in categories {
+        
+        for index in 0..<categories.count {
             let button = UIButton()
-            button.setAttributedTitle(NSAttributedString(string: category, attributes: [.foregroundColor: UIColor.darkGray, .font: UIFont.systemFont(ofSize: 15, weight: .semibold)]), for: .normal)
+            button.setAttributedTitle(NSAttributedString(string: categories[index], attributes: [.foregroundColor: UIColor.darkGray, .font: UIFont.systemFont(ofSize: 15, weight: .semibold)]), for: .normal)
             button.setTitleColor(.darkGray, for: .normal)
             button.backgroundColor = .white
             button.layer.cornerRadius = 15
             button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+            button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
+            
+            button.tag = index
+            button.addTarget(self, action: #selector(categoryButtonTapped), for: .touchUpInside)
+            
             stackView.addArrangedSubview(button)
         }
 
