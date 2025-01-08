33f7a3ca0404f7c712857bc25c1f40bc28d28927
조다은 <thisisdana49@gmail.com>
2025-01-08 23:23:12 +0900

    design: 메인 화면 AutoLayout으로 구현

--- Assignment/Week03/ClapGame369/ClapGame369/ViewController.swift
+++ Assignment/Week03/ClapGame369/ClapGame369/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var resultTextView: UITextView!
+    @IBOutlet var totalNumLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUI()
     }
-
-
+    
+    private func setUI() {
+        titleLabel.text = "369 게임"
+        titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        titleLabel.textAlignment = .center
+        
+        numberTextField.placeholder = "최대 숫자를 입력해 주세요."
+//        numberTextField.color
+        
+        resultTextView.text = "결과는 과연? 두구두구~🥁"
+        resultTextView.font = UIFont.systemFont(ofSize: 22, weight: .regular)
+        resultTextView.textColor = .systemGray3
+        resultTextView.textAlignment = .center
+        
+        totalNumLabel.text = "몇 번이나 박수를 쳐야할까요?"
+        totalNumLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        totalNumLabel.textAlignment = .center
+        totalNumLabel.numberOfLines = 0
+    }
 }
 
