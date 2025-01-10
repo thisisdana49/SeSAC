b254fbfd008f4dc59b9051d35b4ff31582122df1
조다은 <thisisdana49@gmail.com>
2025-01-10 09:59:27 +0900

    feat: Content Hugging Priority, Compression Resistance Priority 관련 실습

---
+++ Practice/Week03/MyThirdApp/MyThirdApp/MyChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyChatTableViewCell.swift
+//  MyThirdApp
+//
+//  Created by 조다은 on 1/10/25.
+//
+
+import UIKit
+
+class MyChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
