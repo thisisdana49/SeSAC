539456c3cc609cf44ced66d94f505541b899b447
조다은 <thisisdana49@gmail.com>
2025-01-11 23:31:02 +0900

    chore: 프로젝트 생성 및 기본 세팅 작업 #31

---
+++ Assignment/Week03/TravelTalk/TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by 조다은 on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
