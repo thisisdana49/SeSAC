e39c0d3d095f95f63fdf828eecb2d48489bc730f
조다은 <thisisdana49@gmail.com>
2025-01-12 21:54:53 +0900

    feat: UISearchBar를 활용하여 사용자 검색 및 검색 결과에 해당하는 사용자만 목록으로 보여주기 #31

--- Assignment/Week03/TravelTalk/TravelTalk/ChatListViewController.swift
+++ Assignment/Week03/TravelTalk/TravelTalk/ChatListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ChatListViewController: UIViewController {
     let chatLists: [ChatRoom] = mockChatList
+    var filteredChatLists: [ChatRoom] = []
+    var isSearching: Bool = false
     
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var collectionView: UICollectionView!
@@ -19,6 +21,8 @@ class ChatListViewController: UIViewController {
         configureCollectionView()
         configureListCollectionViewLayout()
         setUI()
+        
+        searchBar.delegate = self
     }
     
     private func configureCollectionView() {
@@ -54,15 +58,19 @@ class ChatListViewController: UIViewController {
 // MARK: CollectionView Delegate, DataSource
 extension ChatListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return chatLists.count
+        return isSearching ? filteredChatLists.count : chatLists.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ChatListCollectionViewCell.identifier, for: indexPath) as? ChatListCollectionViewCell else { return UICollectionViewCell() }
-        let item = chatLists[indexPath.item]
+        let item = isSearching ? filteredChatLists[indexPath.item] : chatLists[indexPath.item]
         
         cell.configureData(
-            image: item.chatroomImage.first ?? "", name: item.chatroomName, date: item.chatList.last?.date ?? "", message: item.chatList.last?.message ?? "")
+            image: item.chatroomImage.first ?? "",
+            name: item.chatroomName,
+            date: item.chatList.last?.date ?? "",
+            message: item.chatList.last?.message ?? ""
+        )
         
         DispatchQueue.main.async {
             cell.roomImageView.layer.cornerRadius = cell.roomImageView.frame.height / 2
@@ -82,6 +90,32 @@ extension ChatListViewController: UICollectionViewDelegate, UICollectionViewData
         
         navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+// MARK: SearchBar Delegate
+extension ChatListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            print("is empty")
+            isSearching = false
+            filteredChatLists.removeAll()
+            
+        } else {
+            print("is not empty")
+            isSearching = true
+            filteredChatLists = chatLists.filter {
+                $0.chatroomName.contains(searchText)
+                || $0.chatroomImage.contains(searchText)
+            }
+        }
+        collectionView.reloadData()
+    }
     
-    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        isSearching = false
+        filteredChatLists.removeAll()
+        collectionView.reloadData()
+        searchBar.resignFirstResponder()
+    }
 }
