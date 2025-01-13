db9a32111dd7d2949cb3ec27c376a6f48cc7aaec
조다은 <thisisdana49@gmail.com>
2025-01-13 20:11:08 +0900

    feat: 두번째 화면 코드베이스로 레이아웃 구성 #34

--- Assignment/Week04/CodeBaseUIKit/CodeBaseUIKit/SecondViewController.swift
+++ Assignment/Week04/CodeBaseUIKit/CodeBaseUIKit/SecondViewController.swift
@@ -6,24 +6,174 @@
 //
 
 import UIKit
+import SnapKit
 
 class SecondViewController: UIViewController {
 
+    let tabButtonOne = {
+        let button = UIButton()
+        button.setTitle("멤버십", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.backgroundColor = .naverBlack
+//        button.layer.cornerRadius = 15
+        
+        return button
+    }()
+    let tabButtonTwo = {
+        let button = UIButton()
+        button.setTitle("현장결제", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .darkGray
+        button.layer.cornerRadius = 20
+        
+        return button
+    }()
+    
+    let tabButtonThree = {
+        let button = UIButton()
+        button.setTitle("쿠폰", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.backgroundColor = .naverBlack
+//        button.layer.cornerRadius = 15
+        
+        return button
+    }()
+    
+    let payView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 20
+        
+        return view
+    }()
+    
+    let logoImageView = {
+        let imageView = UIImageView()
+        imageView.image = .naverLogo
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    let closeButton = {
+        let button = UIImageView()
+        let image = UIImage(systemName: "xmark")?.withTintColor(.black).withRenderingMode(.alwaysOriginal)
+        button.image = image
+        button.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    let contentImageView = {
+        let imageView = UIImageView()
+        imageView.image = .naverLock
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    let contentLabel = {
+        let label = UILabel()
+        label.text = "한 번만 인증하고\n비밀번호 없이 결제하세요"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        return label
+    }()
+    
+    let confirmButton = {
+        let button = UIButton()
+        button.setTitle("확인", for: .normal)
+        button.backgroundColor = .naverPrimary
+        button.layer.cornerRadius = 25
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureLayout() {
+        view.backgroundColor = .naverBlack
+        
+        configureTabButtons()
+        configurePayView()
     }
-    */
+    
+    func configureTabButtons() {
+        view.addSubview(tabButtonTwo)
+        view.addSubview(tabButtonOne)
+        view.addSubview(tabButtonThree)
+        
+        tabButtonTwo.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(64)
+            make.width.equalTo(120)
+            make.height.equalTo(40)
+        }
+        
+        tabButtonOne.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(64)
+            make.width.equalTo(120)
+            make.height.equalTo(40)
+        }
+        
+        tabButtonThree.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-20)
+            make.top.equalToSuperview().offset(64)
+            make.width.equalTo(120)
+            make.height.equalTo(40)
+        }
+    }
+    
+    func configurePayView() {
+        view.addSubview(payView)
+        payView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(tabButtonTwo).offset(60)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(460)
+        }
+        
+        payView.addSubview(logoImageView)
+        payView.addSubview(closeButton)
+        payView.addSubview(contentImageView)
+        payView.addSubview(contentLabel)
+        payView.addSubview(confirmButton)
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(8)
+            make.leading.equalTo(16)
+            make.width.equalTo(110)
+        }
+        closeButton.snp.makeConstraints { make in
+            make.top.equalTo(20)
+            make.trailing.equalTo(-16)
+            make.size.equalTo(25)
+        }
+        contentImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(payView)
+            make.centerY.equalTo(payView).offset(-80)
+            make.width.equalTo(140)
+        }
+        contentLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(payView)
+            make.centerY.equalTo(payView).offset(20)
+        }
+        confirmButton.snp.makeConstraints { make in
+            make.centerX.equalTo(payView)
+            make.bottom.equalTo(payView).offset(-16)
+            make.horizontalEdges.equalTo(payView).inset(16)
+            make.height.equalTo(50)
+        }
+    }
+}
 
+#Preview {
+    SecondViewController()
 }
