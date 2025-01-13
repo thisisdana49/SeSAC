5261c9ca286ed411427b03224b0c06d7067f7c4f
조다은 <thisisdana49@gmail.com>
2025-01-13 17:30:29 +0900

    feat: Main에 코드 베이스로 버튼 구현 및 추가 #34

--- Assignment/Week04/CodeBaseUIKit/CodeBaseUIKit/ViewController.swift
+++ Assignment/Week04/CodeBaseUIKit/CodeBaseUIKit/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let buttonOne = {
+        let button = UIButton()
+        button.setTitle("Scene 1", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    let buttonTwo = {
+        let button = UIButton()
+        button.setTitle("Scene 2", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    let buttonThree = {
+        let button = UIButton()
+        button.setTitle("Scene 3", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureButtons()
     }
 
 
+    func configureButtons() {
+        let buttonLists = [buttonOne, buttonTwo, buttonThree]
+        
+        for (index, button) in buttonLists.enumerated() {
+            view.addSubview(button)
+            
+            button.snp.makeConstraints { make in
+                make.width.equalTo(100)
+                make.height.equalTo(45)
+                make.centerX.equalTo(view.safeAreaLayoutGuide)
+                make.top.equalTo(300 + (80 * index))
+            }
+        }
+    }
 }
 
+#Preview {
+    ViewController()
+}
