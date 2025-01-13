974a65c8d54c324f7f55d9e89cae4f7e3a62656b
조다은 <thisisdana49@gmail.com>
2025-01-13 10:49:42 +0900

    feat: NSLayoutConstraint 관련 실습

--- Practice/Week04/MyFourthProject/MyFourthProject/ViewController.swift
+++ Practice/Week04/MyFourthProject/MyFourthProject/ViewController.swift
@@ -17,11 +17,57 @@ import UIKit
 class ViewController: UIViewController {
     // 1. Outlet 연결 대신 클래스 인스턴스를 생성해서 담아줌 - 뷰 객체 프로퍼티 ㅅ너언
     let emailTextField = UITextField()
+    let passwordTextField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         frameBasedLayout()
+        
+        view.addSubview(passwordTextField)
+        // Convert From Auto resizing to Constraints
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        let top = NSLayoutConstraint(item: passwordTextField,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: view.safeAreaLayoutGuide,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 50
+        )
+        top.isActive = true
+        
+        let leading = NSLayoutConstraint(item: passwordTextField,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view.safeAreaLayoutGuide,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 40
+        )
+        leading.isActive = true
+        
+        let trailing = NSLayoutConstraint(item: passwordTextField,
+                                          attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: view.safeAreaLayoutGuide,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -40
+        )
+        trailing.isActive = true
+        
+        let height = NSLayoutConstraint(item: passwordTextField,
+                                        attribute: .height,
+                           relatedBy: .equal,
+                                        toItem: view.safeAreaLayoutGuide,
+                           attribute: .height,
+                           multiplier: 1,
+                           constant: 50
+        )
+        height.isActive = true
+        passwordTextField.backgroundColor = .yellow
     }
     
     func frameBasedLayout() {
