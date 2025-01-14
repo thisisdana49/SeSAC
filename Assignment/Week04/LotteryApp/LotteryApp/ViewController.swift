6b910c264e742da03918e3b72a18eb019f135236
조다은 <thisisdana49@gmail.com>
2025-01-14 14:31:22 +0900

    chore: 프로젝트 생성 및 기본 세팅, 필요한 패키지 설치 #35

---
+++ Assignment/Week04/LotteryApp/LotteryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LotteryApp
+//
+//  Created by 조다은 on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
