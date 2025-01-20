b5538cb5bad15d9df00633adc86f1cc2009ea4fa
조다은 <thisisdana49@gmail.com>
2025-01-20 09:53:56 +0900

    chore: 프로젝트 생성 및 초기 세팅

---
+++ Practice/Week05/MyFifthProject/MyFifthProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFifthProject
+//
+//  Created by 조다은 on 1/20/25.
+//
+
+import UIKit
+
+
+/*
+ 1. GCD vs Swift Concurrency
+    - GCD: sync/async/serial/concurrent -> dispatchgroup
+ */
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
