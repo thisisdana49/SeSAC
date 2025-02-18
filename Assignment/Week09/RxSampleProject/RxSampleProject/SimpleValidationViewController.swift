4e67f10462b2c786ca80a7266cdbf4ef25d4d55c
조다은 <thisisdana49@gmail.com>
2025-02-18 22:54:03 +0900

    chore: SimpleValidationViewController 생성

---
+++ Assignment/Week09/RxSampleProject/RxSampleProject/SimpleValidationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleValidationViewController.swift
+//  RxSampleProject
+//
+//  Created by 조다은 on 2/18/25.
+//
+
+import UIKit
+
+class SimpleValidationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
