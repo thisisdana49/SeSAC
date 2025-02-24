35bd8c86c22ed9337d53f7b9aba17f45618ec108
조다은 <thisisdana49@gmail.com>
2025-02-24 17:36:02 +0900

    chore: 프로젝트 초기 셋팅 및 필요한 패키지 추가

---
+++ Assignment/Week10/EveryLotto/EveryLotto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EveryLotto
+//
+//  Created by 조다은 on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
